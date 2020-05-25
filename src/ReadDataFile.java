7d0ab4746462419968ca96d9b1c0c966d7b8b584
soffik <soffik1996@gmail.com>
2020-05-25 14:48:55 +0200

    Changed method readFile to use a map instead of list to store each node. Also added to clode scanner

--- src/ReadDataFile.java
+++ src/ReadDataFile.java
@@ -1,6 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class ReadDataFile {
@@ -8,51 +8,41 @@ public class ReadDataFile {
 
     public static Graph readFile(String fileName) {
         Graph graph = new Graph();
+        Scanner scanner = null;
+
+        HashMap<String, Integer> map = new HashMap<>();
 
         try{
 
             File file = new File(fileName);
-            Scanner scanner = new Scanner(file);
+            scanner = new Scanner(file);
 
             int num = 0;
 
-            ArrayList<String> nodes = new ArrayList<>();
-
             while (scanner.hasNextLine()) {
-
-                int node1;
-                int node2;
 
                 String line = scanner.nextLine();
                 String[] split = line.split(" ");
 
-                if (!nodes.contains(split[0])) {
-                    nodes.add(split[0]);
-                    node1 = num;
-                    num += 1;
-                } else {
-                    node1 = nodes.indexOf(split[0]);
-                }
-
-                if (!nodes.contains(split[1])) {
-                    nodes.add(split[1]);
-                    node2 = num;
-                    num += 1;
-                } else {
-                    node2 = nodes.indexOf(split[1]);
-                }
+                map.putIfAbsent(split[0], num);
+                num += 1;
+                map.putIfAbsent(split[1], num);
+                num += 1;
 
                 // method addVertex only add vertex if it is not in graph
-                graph.addVertex(new Vertex(node1));
-                graph.addVertex(new Vertex(node2));
+                graph.addVertex(new Vertex(map.get(split[0])));
+                graph.addVertex(new Vertex(map.get(split[1])));
 
-                graph.addEdge(node1, node2);
+                graph.addEdge(map.get(split[0]), map.get(split[1]));
+
             }
 
         } catch (FileNotFoundException e) {
             System.err.println("File " + fileName + " not found.");
         } catch (Exception exc) {
             System.err.println(exc.getMessage());
+        } finally {
+            scanner.close();
         }
 
         return graph;
